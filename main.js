801563d3766b5732d2cb7466dc406269bea473a5
Marcos <marcosjfr3@gmail.com>
2024-11-30 16:06:00 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+// VARIABLES
+    // let MyName;
+    // MyName ="josefino";
+
+    // let MyAge = 15
+
+// let MyName = "josefino2",
+//     MyAge = 15,
+//     MyMessage = "hello";
+
+// alert(MyName);
+// alert(MyAge);
+// alert(MyMessage);
+
+
+// CONSTANTES
+const MyBirthDay = "December 17, 2005";
+
+// alert(MyBirthDay);
+
+// TIPOS DE DATOS
+let MyAge = 33;
+let IsActive = true;
+let IsBlue = false;
+
+let MyHeight = null;
+let MyHeight2 = undefined;
+
+// PROMPTS
+// let year = prompt('anyo actual:');
+    // alert(year);
+
+// CONFIRM
+// let IsBoss = confirm("Eres el jefe?");
+    // alert(IsBoss)
+
+// OPERADORES MATEMATICOS
+let Num1 = 10;
+let Num2 = 20;
+
+// alert( Num1 + Num2 );
+// alert ( Num1 - Num2 );
+// alert ( Num1 * Num2 );
+// alert ( Num1 / Num2 );
+
+let Num3 = 2;
+Num3++;
+alert(Num3);
+
+// OPERADORES DE COMPARACION
+alert( 2 > 1 );
+alert( 2 < 1 );
+alert( 2 == 1 );
+alert( 2 != 1 );
